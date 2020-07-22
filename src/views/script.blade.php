2df70b74e4bb44727853bbab4c910eec95a3a5ad
walter bamert <walter@autoitinc.com>
2020-07-22 16:50:33 -0500

    fc0722b

--- src/views/script.blade.php
+++ src/views/script.blade.php
@@ -3,7 +3,15 @@
 @endif
 
 <script type="text/javascript">
-    jQuery(document).ready(function () {
-        jQuery('#{{ $id }}').fullCalendar({!! $options !!});
+    document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById({{ $id }});
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            {!! $options !!}
+        });
+        calendar.render();
     });
+    
+    /*jQuery(document).ready(function () {
+        jQuery('#{{ $id }}').fullCalendar({!! $options !!});
+    });*/
 </script>

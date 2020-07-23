82a71effe9b764174943175df1c1bf1e8f324eae
walter bamert <walter@autoitinc.com>
2020-07-23 17:24:48 -0500

    set up repo to be replaced

--- src/views/files.blade.php
+++
@@ -1,7 +0,0 @@
-<!-- fullcalendar css -->
-<link href="{{ asset('css/fullcalendar.css') }}" rel="stylesheet">
-<!-- moment js -->
-<script src="{{ asset('js/moment.js') }}"></script>
-<!-- fullcalendar js -->
-<script src="{{ asset('js/fullcalendar.js') }}"></script>
-<script src="{{ asset('js/locale-all.js') }}"></script>
